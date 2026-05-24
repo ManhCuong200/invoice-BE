9ffc30eb68c6599d43b292b59d3b7f3a02c380dd
ManhCuong200 <tmanhcuong2k4@gmail.com>
2026-05-24 12:58:58 +0700

    add CORS connect FE

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // --- BẬT CẤU HÌNH CORS ĐỂ FRONTEND GỌI ĐƯỢC API ---
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Cho phép domain Frontend truy cập
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Nếu bạn có truyền cookie hoặc token đi kèm
+  });
+
+  await app.listen(3001); // Giả sử Backend chạy cổng 3001 để nhường cổng 3000 cho Next.js
 }
 bootstrap();
